d744408fe0411f9aa6cdde90a5b8c702699e54ae
ryandark884-gif <ryandark884@gmail.com>
2026-03-08 23:20:07 -0300

    Initialize Discord bot with message handling

---
+++ Index.js
@@ -0,0 +1,24 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot ligado como ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("🏓 Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
