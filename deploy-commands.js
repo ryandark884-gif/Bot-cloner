20acf5e91315611527363c1f6dcf16322abfd8d8
ryandark884-gif <ryandark884@gmail.com>
2026-03-08 23:39:31 -0300

    Add Discord command registration script

---
+++ deploy-commands.js
@@ -0,0 +1,22 @@
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("painel")
+    .setDescription("Abrir painel de controle")
+].map(cmd => cmd.toJSON());
+
+const rest = new REST({ version: "10" }).setToken("SEU_TOKEN");
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationCommands("SEU_CLIENT_ID"),
+      { body: commands }
+    );
+
+    console.log("Comando registrado!");
+  } catch (err) {
+    console.error(err);
+  }
+})();
