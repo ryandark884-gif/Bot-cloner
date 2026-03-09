4d983ddb5270a2cfe0a8afcb51de391c5f92b705
ryandark884-gif <ryandark884@gmail.com>
2026-03-08 23:58:14 -0300

    Fix command registration and update description

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,22 +1,24 @@
 const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 
 const commands = [
-  new SlashCommandBuilder()
-    .setName("painel")
-    .setDescription("Abrir painel de controle")
-].map(cmd => cmd.toJSON());
+new SlashCommandBuilder()
+.setName("painel")
+.setDescription("Abrre painel de gerenciamento")
+].map(c => c.toJSON());
 
 const rest = new REST({ version: "10" }).setToken("SEU_TOKEN");
 
 (async () => {
-  try {
-    await rest.put(
-      Routes.applicationCommands("SEU_CLIENT_ID"),
-      { body: commands }
-    );
+try {
 
-    console.log("Comando registrado!");
-  } catch (err) {
-    console.error(err);
-  }
+await rest.put(
+Routes.applicationCommands("SEU_CLIENT_ID"),
+{ body: commands }
+);
+
+console.log("Comando registrado.");
+
+} catch (err) {
+console.error(err);
+}
 })();
